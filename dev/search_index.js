ad4c4af4cc234a82c7823a2cacdee76e40decb4a
Documenter.jl <documenter@juliadocs.github.io>
2021-10-20 09:47:41 +0000

    build based on 1ea4d9c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageXL","category":"page"},{"location":"#DemoPackageXL","page":"Home","title":"DemoPackageXL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageXL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageXL]","category":"page"}]
+}
